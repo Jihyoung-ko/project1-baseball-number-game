364dae6d1e10b3cb3a8fffa9cb86493e5e6800e0
koji <koji4386@gmail.com>
2021-03-16 12:42:40 +0100

    add basic function

--- main.js
+++ main.js
@@ -7,6 +7,43 @@ const winGame = document.querySelector("#win-game");
 const loseGame = document.querySelector("#lose-game");
 const startAgainbtn = document.querySelector("#game-over input");
 
+
 const isThreeDigits = (number) => {
   return number.length === 3;
 }
+
+const getAnswer = () => {
+  let answer = [];
+  while (answer.length < 3) {
+    let randomNumber = Math.floor(Math.random() *10);
+    answer = answer + randomNumber;
+  }
+  return answer;
+}
+
+const validAttempt = (attempts) => {
+  return attempts >= 10;
+}
+
+const isCorrect = (number, answer) => {
+  return number === answer;
+}
+
+
+const strikeNumber = (number, answer) => {
+  let strike = 0;
+  let arrNumber = number.split('');
+
+  arrNumber.map((digit, i) => {
+    if (digit === answer[i]) {
+      strike++;
+    }
+  });
+  console.log(strike);
+  return strike;
+  
+}
+ strikeNumber();
+
+
+
